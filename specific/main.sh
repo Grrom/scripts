c6544c697e651aa6307993909c7b5c1068faa4ea
Jerome <josejeromelalunio@gmail.com>
2022-03-01 18:55:15 +0800

    did stuff

--- specific/main.sh
+++ specific/main.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 alias jjaniserve="jjmountexternal && cd ${EXTERNAL}/anime/ && node anime_server.js"
-alias dual="xrandr --output DP-1 --mode 1600x900 --rate 60 --left-of HDMI-1 --output HDMI-1 --mode 1920x1080 --rate 60 --primary"
-
+alias dual="xrandr --output DP-1 --mode 1600x900 --rate 60 --left-of HDMI-1 --output HDMI-1 --mode 1920x1080 --rate 60 --primary && xrandr --output DP-1 --pos 0x180"
